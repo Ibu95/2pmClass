2eed2a5c95aea6a1b4780ac0d83eb6ed0401ad64
Ibu95 <ibu95.ia@gmail.com>
2023-09-01 15:14:05 +0530

    2 Commit

--- src/test/java/testpack/ClassA.java
+++ src/test/java/testpack/ClassA.java
@@ -8,33 +8,36 @@ import java.util.Map;
 
 public class ClassA {
 	public static void main(String[] args) {
-		Datas d = new Datas(); 
+		Datas d = new Datas();
 		System.out.println(d.getId());
 		System.out.println(d.getName());
-		
-		Datas d1 = new Datas(); 
+
+		Datas d1 = new Datas();
 		d1.setId(100);
 		d1.setName("B");
 		System.out.println(d1.getId());
 		System.out.println(d1.getName());
-		
+
 		List<Datas> l = new ArrayList<>();
-		l.add(d);l.add(d1); 
+		l.add(d);
+		l.add(d1);
 		System.out.println(l.get(0).getId());
 		System.out.println(l.get(1).getId());
-		
-		Map<Integer, Datas> m = new HashMap<Integer, Datas>(); 
-		m.put(1, d); 
-		m.put(2, d1); 
-		
+
+		Map<Integer, Datas> m = new HashMap<Integer, Datas>();
+		m.put(1, d);
+		m.put(2, d1);
+
 		String name = m.get(1).getName();
-		
+
 		Collection<Datas> values = m.values();
 		for (Datas datas : values) {
 			int id = datas.getId();
 		}
-		
+
 		System.out.println("First Change");
+		System.out.println("First Change2");
+		System.out.println("First Change3");
 	}
 
 }
